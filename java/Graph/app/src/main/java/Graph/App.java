a183d35ed3e046eda6b0483cd9823c260ec34e0b
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-11-15 18:27:23 +0200

    challenge37

--- java/Graph/app/src/main/java/Graph/App.java
+++ java/Graph/app/src/main/java/Graph/App.java
@@ -11,19 +11,28 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
+
       Graph myGraph = new Graph();
       myGraph.addNode("8");
-      myGraph.addNode("3");
       myGraph.addNode("1");
+      myGraph.addNode("2");
       myGraph.addNode("9");
-      myGraph.addNode("6");
+      myGraph.addNode("7");
       myGraph.addNode("5");
 
 
-      myGraph.addEdge("8" , "3");
-      myGraph.addEdge("8" , "9");
-      myGraph.addEdge("8" , "1");
-      myGraph.addEdge("6" , "5");
+
+      myGraph.addEdge("8" , "1" , 50);
+      myGraph.addEdge("5" , "1" , 70);
+      myGraph.addEdge("7" , "5", 20);
+      myGraph.addEdge("8" , "9", 100);
+      myGraph.addEdge("8" , "2", 40);
+      String[] trip = {"8" , "1" , "5"};
+      String[] trip2 = {"8" , "5"};
+      String[] trip3 = {"8" , "1" , "5" , "7" , "5" , "1" , "8" , "9"};
+      String[] trip4 = {"8" , "9" , "5" };
+      String[] trip5 = {"8"};
+      String[] trip6 = {"8" , "2"};
 //      System.out.println(myGraph);
 //      System.out.println(myGraph.getNodes());
 //      System.out.println(myGraph.getNeighbors("8"));
@@ -32,8 +41,13 @@ public class App {
 //      System.out.println(myGraph.size());
 
 
+      System.out.println(myGraph.businessTrip("8",trip));
+      System.out.println(myGraph.businessTrip("8",trip2));
+      System.out.println(myGraph.businessTrip("8",trip3));
+
+
       System.out.println(myGraph.breadthFirst("5"));
-      System.out.println(myGraph.breadthFirst("8"));
-      System.out.println(myGraph.breadthFirst("1"));
+//      System.out.println(myGraph.breadthFirst("8"));
+//      System.out.println(myGraph.breadthFirst("1"));
     }
 }
--- java/Graph/app/src/main/java/Graph/Graph.java
+++ java/Graph/app/src/main/java/Graph/Graph.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Graph<T> {
 
   public Map< Node<T> , List<Node<T>>> nodeList= new HashMap<>();
+  public Map<String , Integer> weightList = new HashMap<>();
+
 
   public Graph() {}
 
@@ -14,12 +16,26 @@ public class Graph<T> {
     return newNode;
   }
 
-  public void addEdge(T value1, T value2) {
-    Node<T> varNode1 = new Node<T>(value1);
-    Node<T> varNode2 = new Node<T>(value2);
-      nodeList.get(varNode1).add(varNode2);
-      nodeList.get(varNode2).add(varNode1);
+//  public void addEdge(T value1, T value2 ) {
+//    Node<T> varNode1 = new Node<T>(value1);
+//    Node<T> varNode2 = new Node<T>(value2);
+//      nodeList.get(varNode1).add(varNode2);
+//      nodeList.get(varNode2).add(varNode1);
+//
+//  }
 
+  public void addEdge(T value1, T value2, int weight) {
+    Node<T> node1 = new Node<T>(value1);
+    if (value1.equals(value2)) {
+      nodeList.get(node1).add(node1);
+      weightList.put(value1 + "->" + value1 , 0);
+    } else {
+      Node<T> node2 = new Node<T>(value2);
+      nodeList.get(node1).add(node2);
+      nodeList.get(node2).add(node1);
+      weightList.put(value1 + "->" + value2 , weight);
+      weightList.put(value2 + "->" + value1 , weight);
+    }
   }
 
   public Set<Node<T>> getNodes() {
@@ -58,6 +74,18 @@ public class Graph<T> {
       }
     }
     return nodes;
+  }
+
+  public String businessTrip(T value, T [] arr) {
+    int cost = 0;
+    for (int i = 0; i < arr.length -1; i++) {
+      if (getNeighbors(arr[i]).contains(new Node<>(arr[i+1]))) {
+        cost += weightList.get(arr[i] + "->" + arr[i+1]);
+      } else {
+        return "False, $0";
+      }
+    }
+    return true+", $" + cost ;
   }
 
   @Override
